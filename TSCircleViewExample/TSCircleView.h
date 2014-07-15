afc4ec7ef27150a90ce0ca650ea2daae1242c5e4
Tomasz Szulc <mail@szulctomasz.com>
2014-07-16 00:34:02 +0200

    Class is separated from the project

---
+++ TSCircleViewExample/TSCircleView.h
@@ -0,0 +1,73 @@
+//
+//  TSAnimatedCircleView.h
+//  MKMapViewCircleScalingTest
+//
+//  Created by Tomasz Szulc Prywatny on 15/07/14.
+//  Copyright (c) 2014 Tomasz Szulc. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface TSCircleView : MKCircleView
+
+/**
+ Specifies radius of circle view. First value is equal a radius of circle pass in `initWithCircle:` method.
+ */
+@property (nonatomic, assign) CLLocationDistance radius;
+
+/**
+ Animation duration. Default to 0.8;
+ */
+@property (nonatomic, assign) CGFloat duration;
+
+/**
+ Animation repeat count. Default to `HUGE_VALF` (inifinte)
+ */
+@property (nonatomic, assign) CGFloat repeatCount;
+
+/**
+ Specifies if view is animating.
+ */
+@property (nonatomic, readonly, getter = isAnimating) BOOL animating;
+
+/**
+ Specifies if view is paused.
+ */
+@property (nonatomic, readonly, getter = isPaused) BOOL paused;
+
+/**
+ Specifies if `backgroundView` should be visible when animation is stopped. Default to NO.
+ */
+@property (nonatomic, assign) BOOL shouldShowBackgroundViewOnStop;
+
+/**
+ A background view. Use to subclass.
+ */
+@property (nonatomic, strong, readonly) UIView *backgroundView;
+
+/**
+ Subclass to use non-default animation. Default is changing opacity and scale.
+ */
+@property (nonatomic, strong, readonly) id animation;
+
+/**
+ Starts animating
+ */
+- (void)startAnimating;
+
+/**
+ Stops animating
+ */
+- (void)stopAnimating;
+
+/**
+ Pause animation
+ */
+- (void)pauseAnimation;
+
+/**
+ Resume animation
+ */
+- (void)resumeAnimation;
+
+@end
---
+++ TSCircleViewExample/TSCircleView.m
@@ -0,0 +1,151 @@
+//
+//  TSAnimatedCircleView.m
+//  MKMapViewCircleScalingTest
+//
+//  Created by Tomasz Szulc Prywatny on 15/07/14.
+//  Copyright (c) 2014 Tomasz Szulc. All rights reserved.
+//
+
+#import "TSCircleView.h"
+
+@implementation TSCircleView
+
+- (id)initWithCircle:(MKCircle *)circle {
+    self = [super initWithCircle:circle];
+    if (self) {
+        _radius = circle.radius;
+        _duration = 0.8;
+        _repeatCount = HUGE_VALF;
+        _animating = NO;
+        _paused = NO;
+        self.shouldShowBackgroundViewOnStop = NO;
+        [self addSubview:self.backgroundView];
+    }
+    return self;
+}
+
+- (void)startAnimating {
+    if (self.isAnimating) {
+        return;
+    }
+    
+    [self _removeAnimation];
+    [self.backgroundView setHidden:NO];
+    [self.backgroundView.layer addAnimation:[self animation] forKey:@"animation"];
+    _animating = YES;
+    _paused = NO;
+}
+
+- (void)stopAnimating {
+    _animating = NO;
+    _paused = NO;
+    [self _removeAnimation];
+    [self.backgroundView setHidden:!self.shouldShowBackgroundViewOnStop];
+}
+
+- (void)pauseAnimation {
+    if (!self.isAnimating || self.isPaused) {
+        return;
+    }
+    
+    _animating = NO;
+    _paused = YES;
+    CFTimeInterval pausedTime = [self.backgroundView.layer convertTime:CACurrentMediaTime() fromLayer:nil];
+    self.backgroundView.layer.speed = 0.0;
+    self.backgroundView.layer.timeOffset = pausedTime;
+}
+
+- (void)resumeAnimation {
+    if (!self.isPaused) {
+        return;
+    }
+    
+    _animating = YES;
+    _paused = NO;
+    CFTimeInterval pausedTime = [self.backgroundView.layer timeOffset];
+    [self _resetLayer];
+    CFTimeInterval timeSincePause = [self.backgroundView.layer convertTime:CACurrentMediaTime() fromLayer:nil] - pausedTime;
+    self.backgroundView.layer.beginTime = timeSincePause;
+}
+
+- (void)_removeAnimation {
+    [self _resetLayer];
+    [self.backgroundView.layer removeAllAnimations];
+}
+
+- (void)_resetLayer {
+    self.backgroundView.layer.speed = 1.0;
+    self.backgroundView.layer.timeOffset = 0.0;
+    self.backgroundView.layer.beginTime = 0.0;
+}
+
+- (void)setShouldShowBackgroundViewOnStop:(BOOL)shouldShowBackgroundViewOnStop {
+    _shouldShowBackgroundViewOnStop = shouldShowBackgroundViewOnStop;
+    [self.backgroundView setHidden:!_shouldShowBackgroundViewOnStop];
+}
+
+- (id)animation {
+    /// animate opacity
+    CABasicAnimation *opacityAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacityAnimation.duration = self.duration;
+    opacityAnimation.repeatCount = self.repeatCount;
+    opacityAnimation.fromValue = [NSNumber numberWithFloat:1.0];
+    opacityAnimation.toValue = [NSNumber numberWithFloat:0.025];
+
+    /// animate resizing
+    CABasicAnimation *transformAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    transformAnimation.duration = self.duration;
+    transformAnimation.repeatCount = self.repeatCount;
+    transformAnimation.fromValue = [NSNumber numberWithFloat:0.6];
+    transformAnimation.toValue = [NSNumber numberWithFloat:1.1];
+    
+    /// group them
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    
+    [group setAnimations:@[opacityAnimation, transformAnimation]];
+    group.duration = self.duration;
+    group.repeatCount = self.repeatCount;
+    
+    return group;
+}
+
+@synthesize backgroundView = _backgroundView;
+- (UIView *)backgroundView {
+    if (!_backgroundView) {
+        _backgroundView = [[UIView alloc] initWithFrame:self.bounds];
+        [_backgroundView setBackgroundColor:[UIColor blackColor]];
+    }
+    return _backgroundView;
+}
+
+#pragma mark - Drawing
+- (void)drawMapRect:(MKMapRect)mapRect zoomScale:(MKZoomScale)zoomScale inContext:(CGContextRef)ctx {
+    MKMapPoint circleCenter = MKMapPointForCoordinate([[self overlay] coordinate]);
+    
+    double radius = self.radius;
+    double mapRadius = radius * MKMapPointsPerMeterAtLatitude([[self overlay] coordinate].latitude);
+    
+    /// calculate rect on map
+    MKMapRect recalculatedRect = MKMapRectMake(circleCenter.x - mapRadius, circleCenter.y - mapRadius, mapRadius * 2, mapRadius * 2);
+    
+    /// calculate normal rect
+    CGRect rect = [self rectForMapRect:recalculatedRect];
+    
+    @synchronized(self) {
+        if(self.backgroundView) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                /// call setNeedsDisplay only when rect of backgroundView is changing from CGRectZero to different
+                BOOL shouldCallNeedsDisplay = NO;
+                if (CGRectEqualToRect(self.backgroundView.frame, CGRectZero)) {
+                    shouldCallNeedsDisplay = YES;
+                }
+                self.backgroundView.frame = rect;
+                if (shouldCallNeedsDisplay) {
+                    [self.backgroundView setNeedsDisplay];
+                }
+            });
+        }
+    }
+}
+
+@end
