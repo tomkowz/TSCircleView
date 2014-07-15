c6cd045db19dc2e39f64e84ac4586d779ee6780d
Tomasz Szulc <mail@szulctomasz.com>
2014-07-16 00:36:18 +0200

    Cleanup

--- TSCircleViewExample/TSCircleView.h
+++ TSCircleViewExample/TSCircleView.h
@@ -1,6 +1,5 @@
 //
-//  TSAnimatedCircleView.h
-//  MKMapViewCircleScalingTest
+//  TSCircleView.h
 //
 //  Created by Tomasz Szulc  on 15/07/14.
 //  Copyright (c) 2014 Tomasz Szulc. All rights reserved.
--- TSCircleViewExample/TSCircleView.m
+++ TSCircleViewExample/TSCircleView.m
@@ -1,6 +1,5 @@
 //
-//  TSAnimatedCircleView.m
-//  MKMapViewCircleScalingTest
+//  TSCircleView.m
 //
 //  Created by Tomasz Szulc  on 15/07/14.
 //  Copyright (c) 2014 Tomasz Szulc. All rights reserved.
